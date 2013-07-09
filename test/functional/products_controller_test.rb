3f422a0e6bf878e41f5030ea72660f8077325b05
dianxia <834102823@qq.com>
2013-07-09 15:03:31 +0800

    depot

---
+++ test/functional/products_controller_test.rb
@@ -0,0 +1,67 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionController::TestCase
+  setup do
+    @product = products(:one)
+	@update = {
+	title: 'Lorem Ipsum',
+	description: 'Wibbles are fun!',
+	image_url: 'lorem.jpg',
+	price: 19.95
+	}
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:products)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create product" do
+    assert_difference('Product.count') do
+      #post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+	  post:create,product:@update
+    end
+
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  test "should show product" do
+    get :show, id: @product
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @product
+    assert_response :success
+  end
+
+  test "should update product" do
+    #put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+	put:update,id:@product,product:@update
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete :destroy, id: @product
+    end
+
+    assert_redirected_to products_path
+  end
+  
+test "product is not valid without a unique title" do
+product = Product.new(title: products(:ruby).title,
+description:"yyy",
+price:1,
+image_url:"fred.gif")
+assert !product.save
+assert_equal "has already been taken",product.errors[:title].join(';')
+end
+  
+end
---
+++ test/unit/product_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+fixtures:products
+  # test "the truth" do
+  #   assert true
+  # end
+end

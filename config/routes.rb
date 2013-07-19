ff47b9113d95a45f72689487a1837695297ab8b0
yangxinxiu <237466817@qq.com>
2013-07-19 08:40:24 +0800

    Add by YangXinxiu

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,40 @@
 Depot::Application.routes.draw do
   get 'admin' => 'admin#index'
 
+
+  #get "sessions/new"
+
+  #get "sessions/create"
+
+  #get "sessions/destroy"
   controller :sessions do
       get 'login' => :new
       post 'login' => :create
       delete 'logout' => :destroy
   end
 
-  #get "sessions/new"
+  scope '(:locale)' do
 
-  #get "sessions/create"
-
- # get "sessions/destroy"
-
-  resources :users
+    resources :users
 
 
-  resources :orders
+    resources :orders
 
 
-  resources :line_items
+    resources :line_items
 
 
-  resources :carts
+    resources :carts
 
 
-  get "store/index"
+    #get "store/index"
 
-  resources :products do
-    get :who_bought, on: :member
+    resources :products do
+      get :who_bought, on: :member
+    end
+
+    root to: 'store#index', as: 'store'
   end
-
-  root to: 'store#index', as: 'store'
-
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
